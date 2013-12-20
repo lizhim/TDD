2a72c9c6d33b9fd9316528ce97bcb2a6f0ebce00
lizhim <dachuan212@163.com>
2013-12-20 13:14:14 +0800

    Creat a four-digit random number的测试完成

---
+++ spec/CreateSpec.js
@@ -0,0 +1,23 @@
+describe("Create a four-digit random number",function(){
+    it("should be four-digit",function(){
+        var counter=create_counter();
+        expect(counter.length).toBe(4);
+    });
+    it("should be not repeat number",function(){
+        var counter=create_counter();
+        var repeat=judge_number_repeat_or_not(counter);
+        expect(repeat).not.toEqual(false);
+    });
+    it("should be random",function(){
+        var counter=create_counter();
+        var random=judge_number_random_or_not();
+        expect(random).toEqual(true);
+    });
+    it("should be Integer",function(){
+        var counter=create_counter();
+        expect(isNaN(counter)).toEqual(false)
+    });
+});
+describe("Compare the number of guesses and random number",function(){
+
+})
