425355c375d25f9afe8337a03a794a3cf04dc0c2
lizhim <dachuan212@163.com>
2013-12-24 14:26:35 +0800

    判断生成的随机数每一位是否与其他三位重复

--- src/Create.js
+++ src/Create.js
@@ -11,3 +11,25 @@ function create_random_number () {
     localStorage.guess_time="0";
     return count;
 }
+function is_equal_random_number (random_number, count) {
+    var b = _.some(count, function (list) {
+        return list == random_number;
+    })
+    return b;
+}
+function judge_number_repeat_or_not (random_number) {
+    if(random_number!=''){
+        for (var m = 0; m < 3; m++) {
+            return get_counter_n(random_number, m)
+        }
+    }
+    return false;
+
+}
+function get_counter_n (random_number, m) {
+    for (var n = m + 1; n < 4; n++) {
+        if (random_number[m] == random_number[n]) {
+            return true;
+        }
+    }
+}
